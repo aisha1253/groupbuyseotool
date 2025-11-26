3b2ba6a9dcedde834e08ad35cd52bcf0c7590a02
davidbesy <davidvesy01022005@gmail.com>
2025-11-27 02:26:03 +0500

    changes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,17 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPostgres } from '@prisma/adapter-postgresql'
+import { Pool } from 'pg'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const pool = process.env.DATABASE_URL ? new Pool({ connectionString: process.env.DATABASE_URL }) : undefined
+const adapter = pool ? new PrismaPostgres(pool) : undefined
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  adapter: adapter,
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
